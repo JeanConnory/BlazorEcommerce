a92b963dd88be0b5c9cef13e51fad7ddcae73d41
Jean Connory <michaelrhcp@gmail.com>
2022-03-26 21:06:39 -0300

    55 - products variants, product types m to n relationship and invoke method to change state of component

--- BlazorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -5,5 +5,7 @@
 		Task<ServiceResponse<List<Product>>> GetProductsAsync();
 
 		Task<ServiceResponse<Product>> GetProductAsync(int productId);
+
+		Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
 	}
 }
--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -13,7 +13,9 @@
 		{
 			var response = new ServiceResponse<List<Product>>()
 			{
-				Data = await _context.Products.ToListAsync()
+				Data = await _context.Products
+							.Include(p => p.Variants)
+							.ToListAsync()
 			};
 
 			return response;
@@ -22,7 +24,10 @@
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
 			var response = new ServiceResponse<Product>();
-			var product = await _context.Products.FindAsync(productId);
+			var product = await _context.Products
+							.Include(p => p.Variants)
+							.ThenInclude(v => v.ProductType)
+							.FirstOrDefaultAsync(p => p.Id == productId);
 
 		    if(product == null)
             {
@@ -33,6 +38,19 @@
             {
 				response.Data = product;
             }
+
+			return response;
+        }
+
+        public async Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl)
+        {
+			var response = new ServiceResponse<List<Product>>
+			{
+				Data = await _context.Products
+								.Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+								.Include(p => p.Variants)
+								.ToListAsync()
+			};
 
 			return response;
         }
