3ffd766a5505cfe52ce1ca5d7f59c35b0d9a2307
Jean Connory <michaelrhcp@gmail.com>
2022-03-31 22:44:10 -0300

    60 - search component

--- BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -6,8 +6,14 @@
 
 		List<Product> Products { get; set; }
 
-		Task GetProducts(string? categoryUrl = null);
+        string Message { get; set; }
+
+        Task GetProducts(string? categoryUrl = null);
 
 		Task<ServiceResponse<Product>> GetProduct(int productId);
+
+		Task SearchProducts(string searchText);
+
+		Task<List<string>> GetProductSearchSuggestions(string searchText);
 	}
 }
--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -11,6 +11,8 @@
 
         public List<Product> Products { get; set; } = new List<Product>();
 
+        public string Message { get; set; } = "Loading products...";
+
         public event Action ProductsChanged;
 
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
@@ -29,6 +31,25 @@
                 Products = result.Data;
 
             ProductsChanged.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+
+            if (result != null && result.Data != null)
+                Products = result.Data;
+            if (Products.Count == 0)
+                Message = "No products found.";
+
+            ProductsChanged?.Invoke();
         }
     }
 }
